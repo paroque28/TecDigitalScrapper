6720ae5162afdaa57657f1293dee5c236bfba208
Pablo <paroque28@gmail.com>
2016-09-22 20:18:57 -0600

    creds on csv

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-import requests
-import  time
+import requests, csv, time, os.path
 from lxml import html
+from os.path import expanduser
 
-USERNAME = "2014084649"
-PASSWORD = "6381"
-
+home = expanduser("~")
+DIR =  home + "/TecDigital"
+CRED_FILE = DIR+ "/creds.csv"
 LOGIN_URL = "http://tecdigital.tec.ac.cr/register/"
 URL = "http://tecdigital.tec.ac.cr/dotlrn/?page_num=2"
 
+
 def main():
     session_requests = requests.session()
+
+    #Login
+    login(session_requests)
+
+    # Scrape url
+    result = session_requests.get(URL, headers = dict(referer = URL))
+    tree = html.fromstring(result.content)
+    bucket_names = tree.xpath("//td[@headers='folders_name']")
+
+    print(bucket_names)
+
+
+def login(session_requests):
+    if (os.path.isfile(CRED_FILE)):
+        with open(CRED_FILE) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                username = row[0]
+                password = row[1]
+
+
+    else:
+        username = input("What's your username?")
+        password = input("What's your pin?")
+        if not os.path.exists(DIR):
+            os.makedirs(DIR)
+        with open(CRED_FILE, 'w') as file:
+            writer = csv.writer(file)
+            data = [username,password]
+            writer.writerow(data)
 
     # Get login csrf token
     result = session_requests.get(LOGIN_URL)
@@ -19,26 +50,20 @@ def main():
 
     # Create payload
     payload = {
-        "username": USERNAME, 
-        "password": PASSWORD, 
+        "username": username,
+        "password": password,
         "token_id": authenticity_token,
-        "hash" : hashcode,
-        "time" : str (int(time.time())),
+        "hash": hashcode,
+        "time": str(int(time.time())),
         "return_url": "/dotlrn/index",
-        "formbutton:style" : "Entrar",
-        "form:id" : "login",
-        "from:mode" : "edit"
+        "formbutton:style": "Entrar",
+        "form:id": "login",
+        "from:mode": "edit"
     }
 
     # Perform login
-    result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
+    result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
 
-    # Scrape url
-    result = session_requests.get(URL, headers = dict(referer = URL))
-    tree = html.fromstring(result.content)
-    bucket_names = tree.xpath("//td[@headers='folders_name']/a/text()")
-
-    print(bucket_names)
 
 if __name__ == '__main__':
     main()
